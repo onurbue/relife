e638b0a0a3e3991b2a75bea2847bc28e45645a2f
Bruno <a25588@alunos.ipca.pt>
2023-05-17 12:47:46 +0100

    Pushed Changes

--- lib/ui/HomePage/homepage.dart
+++ lib/ui/HomePage/homepage.dart
@@ -6,12 +6,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Criar uma appbar num ficheiro fora
       appBar: AppBar(
-        title: const Text('2'),
+        title: const Text('ReLife'),
       ),
-      body: Column(children: const [
-        Text('w'),
+      body: Column(children: [
+        // Missões em Destaque
+        buildMissoesDestaque(),
+        // Missões Normais
+        buildMissoesNormais(),
       ]),
+    );
+  }
+
+  Widget buildMissoesDestaque() {
+    return Container(
+      height: 10,
+      color: Colors.red,
+    );
+  }
+
+  Widget buildMissoesNormais() {
+    return Container(
+      height: 10,
+      color: Colors.green,
     );
   }
 }
