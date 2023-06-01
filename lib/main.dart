cfd7b9528c6fed7c46aa676d0416672508536fe4
Bruno <a25588@alunos.ipca.pt>
2023-06-01 15:08:04 +0100

    Removed comment

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           iconTheme: IconThemeData(
-            color: primaryColor, // Defina a cor desejada aqui
+            color: primaryColor,
           ),
         ),
       ),
