629b1c9c811ddf7d3efd5c25f62583acdd9d3e88
HugoLemos <hugolemosedf2004@gmail.com>
2023-06-21 11:20:03 +0100

    added

--- lib/views/Donation/donation.dart
+++ lib/views/Donation/donation.dart
@@ -3,11 +3,17 @@ import 'package:relife/utils/appbar.dart';
 import 'package:relife/utils/constants.dart';
 import 'package:relife/views/Donation/widgets/header.dart';
 
-class DonationPage extends StatelessWidget {
+class DonationPage extends StatefulWidget {
+  DonationPage({Key? key}) : super(key: key);
+
+  @override
+  _DonationPageState createState() => _DonationPageState();
+}
+
+class _DonationPageState extends State<DonationPage> {
   final _formKey = GlobalKey<FormState>();
   final _amountController = TextEditingController();
-
-  DonationPage({super.key});
+  int _selected = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,72 +30,86 @@ class DonationPage extends StatelessWidget {
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(41)),
             color: const Color.fromRGBO(252, 252, 252, 30),
-            child: Column(children: [
-              Container(
-                margin: const EdgeInsets.all(30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Donation',
-                          style: CustomTextStyles.title,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 25),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        customDonationButton(5, (amount) {
-                          _amountController.text = amount.toString();
-                        }),
-                        const SizedBox(width: 10),
-                        customDonationButton(10, (amount) {
-                          _amountController.text = amount.toString();
-                        }),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        customDonationButton(20, (amount) {
-                          _amountController.text = amount.toString();
-                        }),
-                        const SizedBox(width: 10),
-                        customDonationButton(50, (amount) {
-                          _amountController.text = amount.toString();
-                        }),
-                      ],
-                    ),
-                    Form(
-                      key: _formKey,
-                      child: Column(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          TextFormField(
-                            controller: _amountController,
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              labelText: 'Custom Amount',
-                            ),
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Please, insert an amount';
-                              }
-                              return null;
-                            },
+                          Text(
+                            'Donation',
+                            style: CustomTextStyles.title,
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 25),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          customDonationButton(5, () {
+                            setState(() {
+                              _amountController.text = '5';
+                              _selected = 1;
+                            });
+                          }, _selected == 1),
+                          const SizedBox(width: 10),
+                          customDonationButton(10, () {
+                            setState(() {
+                              _amountController.text = '10';
+                              _selected = 2;
+                            });
+                          }, _selected == 2),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          customDonationButton(20, () {
+                            setState(() {
+                              _amountController.text = '20';
+                              _selected = 3;
+                            });
+                          }, _selected == 3),
+                          const SizedBox(width: 10),
+                          customDonationButton(50, () {
+                            setState(() {
+                              _amountController.text = '50';
+                              _selected = 4;
+                            });
+                          }, _selected == 4),
+                        ],
+                      ),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              controller: _amountController,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                labelText: 'Custom Amount',
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please, insert an amount';
+                                }
+                                return null;
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 50),
-            ]),
+                const SizedBox(height: 50),
+              ],
+            ),
           ),
           const SizedBox(height: 100),
           SizedBox(
@@ -114,32 +134,23 @@ class DonationPage extends StatelessWidget {
   }
 }
 
-Widget customDonationButton(int amount, ValueSetter<int>? onPressed) {
+Widget customDonationButton(
+    int amount, VoidCallback onPressed, bool isSelected) {
   return SizedBox(
     width: 130,
     child: ElevatedButton(
       style: ElevatedButton.styleFrom(
-        backgroundColor: primaryColor,
+        backgroundColor:
+            isSelected ? const Color.fromRGBO(4, 140, 141, 100) : primaryColor,
+        primary: isSelected ? Colors.white : null,
       ),
-      onPressed: () {
-        if (onPressed != null) {
-          onPressed(amount);
-        }
-      },
-      child: Text(amount.toString()),
+      onPressed: onPressed,
+      child: Text(
+        amount.toString(),
+        style: TextStyle(color: Colors.white),
+      ),
     ),
   );
-//   return SizedBox(
-//     width: 130,
-//     child: ElevatedButton(
-//         style: ElevatedButton.styleFrom(
-//           backgroundColor: const Color.fromRGBO(4, 140, 141, 100),
-//         ),
-//         onPressed: () {
-//           print(amount);
-//         },
-//         child: Text(amount.toString())),
-//   );
 }
 
 String customAppText() {
